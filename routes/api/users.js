d6644937f413a56efba9df4bac3a2826bed075db
tkweon1230 <taehkweon@gmail.com>
2021-06-08 00:57:58 -0500

    06-08-2021 changes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,40 @@
 const express = require('express');
+const User = require('../../models/User');
 const router = express.Router();
 
 // @route   GET api/users
-// @desc    Test route
+// @desc    Get all users
 // @access  Public
-router.get('/', (req, res) => res.send('users route'));
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+        
+    } catch (err) {
+        res.status(500).status({ msg: err.message });
+    }
+});
+
+// @route   POST api/users
+// @desc    create a user
+// @access  Public
+router.post('/', async (req, res) => {
+    
+    const { name, email } = req.body;
+
+    let user = new User({
+            name,
+            email
+    });
+
+    try {
+        
+        const newUser = await user.save();
+        res.json(newUser);
+
+    } catch (err) {
+        res.status(400).status({ msg: err.message });
+    }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Init Middleware
+app.use(express.json({ extended: false }) );
+
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
